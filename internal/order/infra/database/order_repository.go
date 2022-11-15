1871ac536b31e23326f654dcf4c1db8ac93cf6fc
Nanzinha <ana.flaviap7@hotmail.com>
2022-11-15 13:34:06 -0300

    chore: prepare db connection and save function

--- internal/order/infra/database/order_repository.go
+++ internal/order/infra/database/order_repository.go
@@ -1,6 +1,9 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"module/internal/order/entity"
+)
 
 type OrderRepository struct {
 	Db *sql.DB
@@ -8,4 +11,20 @@ type OrderRepository struct {
 
 func NewOrderRepository(db *sql.DB) *OrderRepository {
 	return &OrderRepository{Db: db}
+}
+
+/*Método que faz parte do OrderRepository e aguarda a "instância" do struct order*/
+func (r *OrderRepository) Save(order *entity.Order) error {
+	//Preparando a query de inserção
+	stmt, err := r.Db.Prepare("INSERT INTO order (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	//Executando a query
+	/*O "_" significa que é para ignorar a variável de retorno quando executa a query*/
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
 }
