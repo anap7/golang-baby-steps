bc71099fa0f9b75c1b683949e39df874339fc80c
Nanzinha <ana.flaviap7@hotmail.com>
2022-11-15 01:28:35 -0300

    feat: add infra for database

---
+++ internal/order/infra/database/order_repository.go
@@ -0,0 +1,11 @@
+package database
+
+import "database/sql"
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{Db: db}
+}
